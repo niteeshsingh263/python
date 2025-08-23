15281935c96447cfeb04438039fa7697096e9f67
niteeshsingh263 <niteeshsingh263@gmail.com>
2025-08-23 23:29:38 +0530

    Update nested.py

--- loops/nested.py
+++ loops/nested.py
@@ -1,7 +1,7 @@
-"""for i in range (1,4):
+for i in range (1,4):
     for j in range(1,11):
         print(j, end ="")
-    print()"""
+    print()
 
 
 #pattern problem 
@@ -9,4 +9,5 @@
 for i in range (1,11):
     for j in range( 1,11):
         print(i * j, end="   ")
+
     print()
