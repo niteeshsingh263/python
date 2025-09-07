ca3fefdf43857688d90ec2f855e5ad761ecc32d4
niteeshsingh263 <niteeshsingh263@gmail.com>
2025-09-07 14:48:41 +0530

    Add files via upload

---
+++ String Function/String Slicing/slicing.py
@@ -0,0 +1,17 @@
+a = "HArry Potter and the Goblet of Fire"
+
+b = "0123456789"
+
+print(a[0:12])
+
+print(a[6:12])
+
+#gap of number
+print(b[::2])
+
+print(b[3::2])
+
+#reverse ke liye
+print(b[::-1])
+
+print(b[7::-1])
